a5f92ecc7a1efff6679704f4f9faf6ac4ef41cae
Paul Thebault <pthebaul@etud.insa-toulouse.fr>
2020-12-01 19:13:26 +0100

    Fixing merging problems

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,18 @@ import java.util.Map;
 
 public class Main {
 
-    private static final User localUser = new User();
-
     public static void main(String[] args) throws InterruptedException {
         try {
             UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
         } catch (IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException | ClassNotFoundException e) {
             e.printStackTrace();
         }
-
+        // Keys are: "nickname", "localPort", "remoteAddress" and "remotePort"
         Map<String, String> information = WelcomeWindow.askInformation();
 
-        localUser.setNickname(information.get("nickname"));
+        User localUser = new User(information.get("nickname"));
 
-        User remoteUser = new User();
-        remoteUser.setNickname(information.get("remoteAddress"));
+        User remoteUser = new User("Bob");
 
         new ConversationWindow(localUser, remoteUser);
     }
