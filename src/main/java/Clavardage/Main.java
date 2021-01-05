ca3d6c1c00c855ad2d5c99d63a9ccad22aa6aa6a
Paul Thebault <pthebaul@etud.insa-toulouse.fr>
2021-01-05 11:42:26 +0100

    Turned NicknameSelectionWindow into a JOptionPane, and added AlertWindow

--- src/main/java/Clavardage/Main.java
+++ src/main/java/Clavardage/Main.java
@@ -4,8 +4,8 @@ import Clavardage.Managers.ConversationsManager;
 import Clavardage.Managers.MessagesManager;
 import Clavardage.Managers.UsersManager;
 import Clavardage.Models.User;
+import Clavardage.Views.AlertWindow;
 import Clavardage.Views.MainWindow;
-import Clavardage.Views.NicknameSelectionWindow;
 
 import javax.swing.*;
 
@@ -18,7 +18,24 @@ public class Main {
             e.printStackTrace();
         }
 
-        String nickname = NicknameSelectionWindow.askNickname();
+        boolean validNickname = false;
+        String message;
+        String nickname = null;
+        final String title = "Clavardage";
+
+        while (!validNickname) {
+            message = "Welcome! Please enter your nickname:";
+            nickname = JOptionPane.showInputDialog(null, message, title, JOptionPane.PLAIN_MESSAGE);
+
+            if (nickname == null) {
+                // Window closed, or cancel button pressed
+                System.exit(0);
+            } else if (nickname.length() == 0 || nickname.contains(" ")) {
+                AlertWindow.displayError("Your nickname cannot be empty nor contain any spaces");
+            } else {
+                validNickname = true;
+            }
+        }
 
         User.instantiateLocalUser(nickname);
 
---
+++ src/main/java/Clavardage/Views/AlertWindow.java
@@ -0,0 +1,23 @@
+package Clavardage.Views;
+
+import javax.swing.*;
+
+public class AlertWindow {
+    private static final String TITLE = "Clavardage";
+
+    public static void displayInformation(String message) {
+        displayDialog(message, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void displayWarning(String message) {
+        displayDialog(message, JOptionPane.WARNING_MESSAGE);
+    }
+
+    public static void displayError(String message) {
+        displayDialog(message, JOptionPane.ERROR_MESSAGE);
+    }
+
+    private static void displayDialog(String message, int messageType) {
+        JOptionPane.showMessageDialog(MainWindow.INSTANCE, message, AlertWindow.TITLE, messageType);
+    }
+}
